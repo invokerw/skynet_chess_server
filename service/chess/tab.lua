c8f59114fc0fba6dc3aad93f3ae89c401a6d290b
wfatestaynight <784856034@qq.com>
2017-08-08 18:36:38 +0800

    add some check

--- service/chess/tab.lua
+++ service/chess/tab.lua
@@ -9,20 +9,48 @@ function M.new()
 	local o = {
 		id = env.room:get_tab_id()
 	}
-	print("log: table id = 2???  get_tab_id()== ",o.id)
 	setmetatable(o, M)
 	return o
 end
 
 local function print_chessboard(chessboard)
-	for i=1,9 do
-		local s = ""
-		for j=1,9 do
-			s = s..chessboard[i][j].." "
+	local s = ""
+	for i=1,90 do
+		s = s..chessboard[i].." "
+		if i%10 == 0 then
+			print(s)
+			s = ""
 		end
-		print(s)
 	end
 end
+local horse     = {-19,-17,-11,-7, 7,11,17,19}
+local horse_leg = { -9, -9, -1, 1,-1, 1, 9, 9}
+local elephant     = {-20,-16,16,20}
+local elephant_leg = {-10, -8, 8,10}
+local in_palace = {
+	0,  0,  0,  1,  1,  1,  0,  0,  0,
+	0,  0,  0,  1,  1,  1,  0,  0,  0,
+	0,  0,  0,  1,  1,  1,  0,  0,  0,
+	0,  0,  0,  0,  0,  0,  0,  0,  0,
+	0,  0,  0,  0,  0,  0,  0,  0,  0,
+	--     楚河            汉界
+	0,  0,  0,  0,  0,  0,  0,  0,  0,
+	0,  0,  0,  0,  0,  0,  0,  0,  0,
+	0,  0,  0,  1,  1,  1,  0,  0,  0,
+	0,  0,  0,  1,  1,  1,  0,  0,  0,
+	0,  0,  0,  1,  1,  1,  0,  0,  0,	
+}
+--chess_pieces[48]这个扩展的棋子数组比较难以理解，实际上用了“屏蔽位”的设计，即1位表示红子(16)，1位表示黑子(32)。
+--因此0到15没有作用，16到31代表红方棋子(16代表帅，17和18代表仕，依此类推，直到27到31代表兵)，32到47代表黑方棋子(在红方基础上加16)。
+--棋盘数组中的每个元素的意义就明确了，0代表没有棋子，16到31代表红方棋子，32到47代表黑方棋子。
+--好处：它可以快速判断棋子的颜色，(Piece & 16)可以判断是否为红方棋子，(Piece & 32)可以判断是否为黑方棋子。
+local chess_pieces = {
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	86,85,87,84,88,83,89,82,90,65,71,55,57,59,61,63,
+	5,4,6,3,7,2,8,1,9,20,26,28,30,32,34,36
+}
+
+
 function M:init(p1, p2)
 	self.red = p1
 	self.black = p2
@@ -31,16 +59,17 @@ function M:init(p1, p2)
 	--加上棋盘数据
 	--十几的棋子是红方
 	self.chessboard = {
-		 {5, 4, 3, 2, 1, 2, 3, 4, 5},
-		 {0, 0, 0, 0, 0, 0, 0, 0, 0},
-		 {0, 6, 0, 0, 0, 0, 0, 6, 0},
-		 {7, 0, 7, 0, 7, 0, 7, 0, 7},
-		 {0, 0, 0, 0, 0, 0, 0, 0, 0},
-		 {0, 0, 0, 0, 0, 0, 0, 0, 0},
-		 {17, 0, 17, 0, 17, 0, 17, 0, 17},
-		 {0, 16, 0, 0, 0, 0, 0, 16, 0},
-		 {0, 0, 0, 0, 0, 0, 0, 0, 0},
-		 {15, 14, 13, 12, 11, 12, 13, 14, 15}
+		39, 37, 35, 33, 32, 34, 36, 38, 40,
+		 0,  0,  0,  0,  0,  0,  0,  0,  0,
+		 0, 41,  0,  0,  0,  0,  0, 42,  0,
+		43,  0, 44,  0, 45,  0, 46,  0, 47,
+		 0,  0,  0,  0,  0,  0,  0,  0,  0,
+		--     楚河            汉界
+		 0,  0,  0,  0,  0,  0,  0,  0,  0,
+		27,  0, 28,  0, 29,  0, 30,  0, 31,
+		 0, 25,  0,  0,  0,  0,  0, 26,  0,
+		 0,  0,  0,  0,  0,  0,  0,  0,  0,
+		23, 21, 19, 17, 16, 18, 20, 22, 24
 	}
 	--print("self.chessboard:")
 	--print_chessboard(self.chessboard)
@@ -67,14 +96,57 @@ end
 function M:move(info, msg)
 	--解析数据进行移动
 	local chessman = self.chessboard[msg.move.srow][msg.move.scol]
-	self.chessboard[msg.move.srow][msg.move.scol] = 0
-	self.chessboard[msg.move.drow][msg.move.dcol] = chessman
-	--print("self.chessboard:")
-	--print_chessboard(self.chessboard)
+	local source = (msg.move.srow - 1) * 9 + msg.move.scol
+	local direc = (msg.move.drow - 1) * 9 + msg.move.dcol
+	--不该他走
+	if ((chessman & 16) and self.redrun == true) or ((chessman & 32)  and self.redrun == false) then
+		return
+	end
+	--不能移动到自己的棋子上
+	if ((self.chessboard[direc] & 16) and self.redrun == true) or ((self.chessboard[direc] & 32) and self.redrun == false) then
+		return
+	end
+	--首先可不可以这么走
+
+	--可以这么走发送消息
 	skynet.send(self.red.agent, "lua", "send", "Table.MoveNotify", msg)
 	skynet.send(self.black.agent, "lua", "send", "Table.MoveNotify", msg)
+	--是否赢了
+	if checkmate(self.chessboard, direc) then
+		print("is red:"..self.redrun..", Win")
+		
+	end
+	--是否将军
 	
-	self.redrun = !self.redrun
+	--改变服务器棋盘 
+	self.chessboard[source] = 0
+	self.chessboard[direc] = chessman
+	print("self.chessboard:")
+	print_chessboard(self.chessboard)
+
+	
+	self.redrun = not self.redrun
 end
 
+--将军
+local function check(chessboard, source, direc, redrun)
+	if true then
+	end
+end
+
+--赢了
+local function checkmate(chessboard, direc)
+	if chessboard[direc] == 11 or chessboard[direc] == 1 then
+		return true
+	end
+	return false
+end
+
+--可以这么走吗
+local function canmove(chessboard, source, direc, redrun)
+	local chessman = chessboard[source]
+	--如果是
+	if  true then
+	end
+end
 return M
